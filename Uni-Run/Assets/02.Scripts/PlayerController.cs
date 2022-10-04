24f51e16a74dbcdf968226af010d061db13f7a18
yuna237 <wh1854wh@naver.com>
2022-10-04 16:27:01 +0900

    20221004.2

---
+++ Uni-Run/Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioClip deathCip; // 사망 시 재생할 오디오 클립
+    public float jumpForce = 700f; // 점프 힘
+
+    private int jumpCount = 0; // 누적 점프 회수
+    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
+    private bool isDead = false; // 사망 상태
+
+    private Rigidbody2D playerRigidbody; // 사용할 리지드바디 컴포넌트
+    private Animator animator; // 사용할 애니메이터 컴포넌트
+    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //초기화
+        //게임 오브젝트로부터 사용할 컴포넌트들을 가져와 변수에 할당
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator= GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    { //사용자 입력을 감지하고 점프하는 처리
+
+        if (isDead)
+        {
+            //사망처리를 더이상 진행하지 않고 종료
+            return;
+        }
+        //마우스 왼쪽 버튼을 눌렀으며 && 최대 점프 횟수(2)에 도달하지 않았다면
+        if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {
+            //점프 횟수 증가
+            jumpCount++;
+            //점프 직전에 속도를 순간적으로 제로 (0,0)로 변경
+            playerRigidbody.velocity = Vector2.zero;
+            //리지드 바디에 위쪽으로 힘 주기
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            //오디오 소스 재생
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+        {
+            // 마우스 왼쪽 버튼에서 손을 떼는 순간 &&속도의 y 값이 양수라면(위로 상승 중)
+            //현재 속도를 절반으로 변경
+            playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+
+        }
+
+        //애니메이터의 Grounded 피라미터를 isGrounded 값으로 갱신
+        animator.SetBool("Grounded", isGrounded);
+
+    }
+
+    private void Die()
+    {
+        //사망처리
+        //애니메이터의 Die 트리거 피라미터를 셋
+        animator.SetTrigger("Die");
+        //오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+        playerAudio.clip = deathCip;
+        //사망효과음 재생
+        playerAudio.Play();
+       
+        //속도를 제로(0,0)로 변경
+        playerRigidbody.velocity = Vector2.zero;
+        //사망 상태를 true로 변경
+        isDead = true;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //트리거 콜라이더를 가진 장애물과의 충돌을 감지
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //바닥을 닿았음을 감지하는 처리
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //바닥에서 벗어났음을 감지하는 처리
+    }
+}
