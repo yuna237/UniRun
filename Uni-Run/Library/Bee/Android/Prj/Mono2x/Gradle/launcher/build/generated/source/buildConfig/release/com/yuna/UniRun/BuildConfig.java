7ca6302c9cb562bf8c31ffc26eb1cac130189737
yuna237 <wh1854wh@naver.com>
2022-10-12 16:44:24 +0900

    20221012

---
+++ Uni-Run/Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/yuna/UniRun/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yuna.UniRun;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.yuna.UniRun";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
